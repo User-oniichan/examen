21183d1f50e101b4cbce04aad6ee4be6e53bb954
Helber <helbercillocomunica@gmail.com>
2023-11-14 16:46:45 -0500

    Mi primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { C01componente } from './components/C01componente';
+
+function App() {
+  return (
+    <div>
+      <C01componente/>
+      <h2>ARHUIRE SUYCO HELBER ARTURO</h2>
+      <h3>NOCHE</h3>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/C01componente.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const C01componente = () => {
+  return (
+    <div><h1>Instituto Superior Publico Honorio Delgado Espinoza</h1></div>
+  )
+}
